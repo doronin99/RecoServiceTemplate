9d2193d1e2e050806b8379ba7326814b4931082c
Alexander Doronin <doronin99@icloud.com>
2023-11-29 21:56:52 +0700

    Add task3

--- service/api/views.py
+++ service/api/views.py
@@ -1,28 +1,32 @@
 from typing import List
-
 from fastapi import APIRouter, FastAPI, Request
+from models.loader import load_model
 from pydantic import BaseModel
-
 from service.api.exceptions import ModelNotFoundError, UserNotFoundError
 from service.log import app_logger
 
 
+# Define a Pydantic model for the response
 class RecoResponse(BaseModel):
     user_id: int
     items: List[int]
 
 
+# Create an APIRouter instance
 router = APIRouter()
 
 
+# Define a health check endpoint
 @router.get(
     path="/health",
     tags=["Health"],
 )
 async def health() -> str:
+    """Health check endpoint"""
     return "I am alive"
 
 
+# Define a recommendation endpoint
 @router.get(
     path="/reco/{model_name}/{user_id}",
     tags=["Recommendations"],
@@ -33,21 +37,28 @@ async def get_reco(
     model_name: str,
     user_id: int,
 ) -> RecoResponse:
+    """Get recommendations for a user based on the specified model."""
     app_logger.info(f"Request for model: {model_name}, user_id: {user_id}")
 
     k_recs = request.app.state.k_recs
 
+    # Check if the user_id is within a valid range
     if user_id > 10 ** 9:
         raise UserNotFoundError(error_message=f"User {user_id} not found")
 
-    # Write your code here
+    # Retrieve recommendations based on the specified model
     if model_name == "top":
         reco = list(range(k_recs))
+    elif model_name == "userknn":
+        userknn_model = load_model("models/userknn.dill")
+        reco = userknn_model.predict_single(user_id, N_recs=k_recs)
     else:
         raise ModelNotFoundError(error_message=f"Model {model_name} not found")
 
+    # Return the recommendations
     return RecoResponse(user_id=user_id, items=reco)
 
 
+# Function to add the defined views to the FastAPI app
 def add_views(app: FastAPI) -> None:
     app.include_router(router)
